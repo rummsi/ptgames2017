8dd6d23922475e6978eec4e70cc897d59eb04824
rummsi <rummsi@gmail.com>
2015-04-05 10:46:09 +0000

    ADD: Criada classe Database_Mysqli.php, para implementação das funções mysqli.

--- common.php
+++ common.php
@@ -1,4 +1,5 @@
 <?php
+
 /**
  * This file is part of XNova:Legacies
  *
@@ -27,13 +28,11 @@
  * documentation for further information about customizing XNova.
  *
  */
-
 session_start();
 
 if (in_array(strtolower(getenv('DEBUG')), array('1', 'on', 'true'))) {
     define('DEBUG', true);
 }
-
 !defined('DEBUG') || @ini_set('display_errors', false);
 !defined('DEBUG') || @error_reporting(E_ALL | E_STRICT);
 
@@ -47,9 +46,9 @@ if (0 === filesize(ROOT_PATH . 'config.php')) {
     die();
 }
 
-$game_config   = array();
-$user          = array();
-$lang          = array();
+$game_config = array();
+$user = array();
+$lang = array();
 $IsUserChecked = false;
 
 define('DEFAULT_SKINPATH', 'skins/xnova/');
@@ -57,8 +56,10 @@ define('TEMPLATE_DIR', realpath(ROOT_PATH . '/templates/'));
 define('TEMPLATE_NAME', 'OpenGame');
 define('DEFAULT_LANG', 'fr');
 
-include(ROOT_PATH . 'includes/debug.class.'.PHPEXT);
+include(ROOT_PATH . 'includes/debug.class.' . PHPEXT);
 $debug = new Debug();
+include(ROOT_PATH . 'db/Database_Mysqli.php');
+$mysqli = new Database_Mysqli();
 
 include(ROOT_PATH . 'includes/constants.' . PHPEXT);
 include(ROOT_PATH . 'includes/functions.' . PHPEXT);
@@ -70,14 +71,14 @@ include(ROOT_PATH . 'includes/db.' . PHPEXT);
 include(ROOT_PATH . 'includes/strings.' . PHPEXT);
 
 $query = doquery('SELECT * FROM {{table}}', 'config');
-while($row = mysqli_fetch_assoc($query)) {
+while ($row = mysqli_fetch_assoc($query)) {
     $game_config[$row['config_name']] = $row['config_value'];
 }
 
 if (!defined('DISABLE_IDENTITY_CHECK')) {
-    $Result        = CheckTheUser ( $IsUserChecked );
+    $Result = CheckTheUser($IsUserChecked);
     $IsUserChecked = $Result['state'];
-    $user          = $Result['record'];
+    $user = $Result['record'];
 } else if (!defined('DISABLE_IDENTITY_CHECK') && $game_config['game_disable'] && $user['authlevel'] == LEVEL_PLAYER) {
     message(stripslashes($game_config['close_reason']), $game_config['game_name']);
 }
@@ -91,7 +92,7 @@ if (empty($user) && !defined('DISABLE_IDENTITY_CHECK')) {
 }
 
 $now = time();
-$sql =<<<SQL_EOF
+$sql = <<<SQL_EOF
 SELECT
   fleet_start_galaxy AS galaxy,
   fleet_start_system AS system,
@@ -116,7 +117,7 @@ while ($row = mysqli_fetch_array($_fleets)) {
 
 unset($_fleets);
 
-include(ROOT_PATH . 'rak.'.PHPEXT);
+include(ROOT_PATH . 'rak.' . PHPEXT);
 if (!defined('IN_ADMIN')) {
     $dpath = (isset($user['dpath']) && !empty($user["dpath"])) ? $user['dpath'] : DEFAULT_SKINPATH;
 } else {
@@ -127,8 +128,8 @@ if (!defined('IN_ADMIN')) {
 if (!empty($user)) {
     SetSelectedPlanet($user);
 
-    $planetrow = doquery("SELECT * FROM {{table}} WHERE `id` = '".$user['current_planet']."';", 'planets', true);
-    $galaxyrow = doquery("SELECT * FROM {{table}} WHERE `id_planet` = '".$planetrow['id']."';", 'galaxy', true);
+    $planetrow = doquery("SELECT * FROM {{table}} WHERE `id` = '" . $user['current_planet'] . "';", 'planets', true);
+    $galaxyrow = doquery("SELECT * FROM {{table}} WHERE `id_planet` = '" . $planetrow['id'] . "';", 'galaxy', true);
 
     CheckPlanetUsedFields($planetrow);
     PlanetResourceUpdate($user, $planetrow, time());
