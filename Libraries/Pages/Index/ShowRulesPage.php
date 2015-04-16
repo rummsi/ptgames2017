d1e864db97e22b42c2a333ecfa0f84b27177d1b9
rummsi <rummsi@gmail.com>
2015-04-16 17:30:57 +0000

    - Conversão do ShowRulesPage para o Smarty.

--- Libraries/Pages/Index/ShowRulesPage.php
+++ Libraries/Pages/Index/ShowRulesPage.php
@@ -39,13 +39,72 @@ class ShowRulesPage extends AbstractIndexPage {
         global $lang, $game_config;
         includeLang('rules');
 
-        $parse = $lang;
-        $parse['servername'] = $game_config['game_name'];
+        $this->tplObj->assign(array(
+            'title' => $lang['rules'],
+            'rules' => $lang['rules'],
+            'servername' => $game_config['game_name'],
+            'respectrules' => $lang['respectrules'],
+            'Account' => $lang['Account'],
+            'MultiAccount' => $lang['MultiAccount'],
+            'Sitting' => $lang['Sitting'],
+            'Trade' => $lang['Trade'],
+            'Bash' => $lang['Bash'],
+            'Push' => $lang['Push'],
+            'Bugusing' => $lang['Bugusing'],
+            'MailIngame' => $lang['MailIngame'],
+            'OutXnova' => $lang['OutXnova'],
+            'Spam' => $lang['Spam'],
+            'AccountText' => $lang['AccountText'],
+            'AccountText2' => $lang['AccountText2'],
+            'MultiAccountText' => $lang['MultiAccountText'],
+            'MultiAccountText2' => $lang['MultiAccountText2'],
+            'MultiAccountText3' => $lang['MultiAccountText3'],
+            'SittingText' => $lang['rules'],
+            'SittingText2' => $lang['SittingText2'],
+            'SittingText3' => $lang['SittingText3'],
+            'SittingText4' => $lang['SittingText4'],
+            'SittingText5' => $lang['SittingText5'],
+            'SittingText6' => $lang['SittingText6'],
+            'SittingText7' => $lang['SittingText7'],
+            'SittingText8' => $lang['SittingText8'],
+            'SittingText9' => $lang['SittingText9'],
+            'SittingText10' => $lang['SittingText10'],
+            'SittingText11' => $lang['SittingText11'],
+            'SittingText12' => $lang['SittingText12'],
+            'SittingText13' => $lang['SittingText13'],
+            'SittingText14' => $lang['SittingText14'],
+            'SittingText15' => $lang['SittingText15'],
+            'TradeText' => $lang['TradeText'],
+            'TradeText2' => $lang['TradeText2'],
+            'BashText' => $lang['BashText'],
+            'BashText2' => $lang['BashText2'],
+            'exception' => $lang['exception'],
+            'BashExepText' => $lang['BashExepText'],
+            'BashExepText2' => $lang['BashExepText2'],
+            'BashExepText3' => $lang['BashExepText3'],
+            'PushText' => $lang['PushText'],
+            'PushText2' => $lang['PushText2'],
+            'PushText3' => $lang['PushText3'],
+            'PushText4' => $lang['PushText4'],
+            'PushText5' => $lang['PushText5'],
+            'exemple' => $lang['exemple'],
+            'PushEx' => $lang['PushEx'],
+            'PushEx2' => $lang['PushEx2'],
+            'recyclage' => $lang['recyclage'],
+            'PushRec' => $lang['PushRec'],
+            'mercenariat' => $lang['mercenariat'],
+            'PushMer' => $lang['PushMer'],
+            'PushMer2' => $lang['PushMer2'],
+            'BugusingText' => $lang['BugusingText'],
+            'BugusingText2' => $lang['BugusingText2'],
+            'BugusingText3' => $lang['BugusingText3'],
+            'BugusingText4' => $lang['BugusingText4'],
+            'MailIngameText' => $lang['MailIngameText'],
+            'OutText' => $lang['OutText'],
+            'SpamText' => $lang['SpamText'],
+        ));
 
-        $PageTPL = gettemplate('rules_body');
-        $page = parsetemplate($PageTPL, $parse);
-
-        Game::display($page, $lang['rules'], false);
+        $this->render('rules_body.tpl');
     }
 
 }
