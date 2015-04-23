01e2dca2b65a84a0bb7a1ede4b8525e0447574ba
rummsi <rummsi@gmail.com>
2015-04-23 18:44:15 +0000

    FIX: -ShowChangelogPage, ShowOverviewPage.

--- Libraries/Pages/Game/ShowChangelogPage.php
+++ Libraries/Pages/Game/ShowChangelogPage.php
@@ -1,33 +1,34 @@
 <?php
 
-/**
- * This file is part of XNova:Legacies
- *
- * @license http://www.gnu.org/licenses/gpl-3.0.txt
- * @see http://www.xnova-ng.org/
- *
- * Copyright (c) 2009-Present, XNova Support Team <http://www.xnova-ng.org>
- * All rights reserved.
- *
+/*
+ * XNovaPT
+ * Copyright (C) 2012
+ * 
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
  * the Free Software Foundation, either version 3 of the License, or
  * (at your option) any later version.
- *
+ * 
  * This program is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU General Public License for more details.
+ * 
+ * You should read the GNU General Public License, see <http://www.gnu.org/licenses/>.
+ * 
+ * XNovaPT
+ * @author XNovaPT Team <xnovaptteam@gmail.com>
+ * ShowChangelogPage.php
+ * @license http://www.gnu.org/licenses/gpl.html GNU GPLv3 License
+ * @version 0.01  23/abr/2015 19:00:35
+ */
+
+/**
+ * Description of ShowChangelogPage
  *
- * You should have received a copy of the GNU General Public License
- * along with this program.  If not, see <http://www.gnu.org/licenses/>.
- *
- *                                --> NOTICE <--
- *  This file is part of the core development branch, changing its contents will
- * make you unable to use the automatic updates manager. Please refer to the
- * documentation for further information about customizing XNova.
- *
- */class ShowChangelogPage extends AbstractGamePage {
+ * @author author XNovaPT Team <xnovaptteam@gmail.com>
+ */
+class ShowChangelogPage extends AbstractGamePage {
 
     function __construct() {
         parent::__construct();
@@ -35,22 +36,14 @@
     }
 
     function show() {
-        global $lang, $title;
+        global $lang;
         includeLang('changelog');
 
-        foreach ($lang['changelog'] as $a => $b) {
-            $this->tplObj->assign(array(
-                'version_number' => $a,
-                'description' => nl2br($b),
-            ));
-            @$body .= $this->tplObj->fetch('changelog_table.tpl');
-        }
-        
         $this->tplObj->assign(array(
             'title' => 'Change Log',
             'Version' => $lang['Version'],
             'Description' => $lang['Description'],
-            'body' => $body
+            'lang_changelog' => $lang['changelog'],
         ));
 
         $this->render('changelog_body.tpl');
