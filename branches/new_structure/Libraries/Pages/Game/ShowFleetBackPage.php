668a9528b6ab93e027036a4074f215b6d039103b
rummsi <rummsi@fc0ce58d-76a3-41d0-bf4d-8f37abf19d28>
2015-05-02 12:43:27 +0000

    - Criada a class ShowFleetBackPage. - Criada a class ShowFleetPage. - Criada a class ShowFleetshortcutPage. - Criada a class ShowFloten1Page. - Criada a class ShowFloten2Page. - Criada a class ShowFloten3Page. - Criada a class ShowFlotenajaxPage.

--- branches/new_structure/Libraries/Pages/Game/ShowFleetBackPage.php
+++ branches/new_structure/Libraries/Pages/Game/ShowFleetBackPage.php
@@ -1,87 +1,96 @@
 <?php
 
-/**
- * This file is part of XNova:Legacies
- *
- * @license http://www.gnu.org/licenses/gpl-3.0.txt
- * @see http://www.xnova-ng.org/
- *
- * Copyright (c) 2009-Present, XNova Support Team <http://www.xnova-ng.org>
- * All rights reserved.
- *
+/*
+ * XNovaPT
+ * Copyright (C) 2012
+ * 
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
  * the Free Software Foundation, either version 3 of the License, or
  * (at your option) any later version.
- *
+ * 
  * This program is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License
- * along with this program.  If not, see <http://www.gnu.org/licenses/>.
- *
- *                                --> NOTICE <--
- *  This file is part of the core development branch, changing its contents will
- * make you unable to use the automatic updates manager. Please refer to the
- * documentation for further information about customizing XNova.
- *
+ * 
+ * You should read the GNU General Public License, see <http://www.gnu.org/licenses/>.
+ * 
+ * XNovaPT
+ * @author XNovaPT Team <xnovaptteam@gmail.com>
+ * @ShowFleetBackPage.php
+ * @license http://www.gnu.org/licenses/gpl.html GNU GPLv3 License
+ * @version 0.01  2/mai/2015 13:29:31
  */
-includeLang('fleet');
 
-$BoxTitle = $lang['fl_error'];
-$TxtColor = "red";
-$BoxMessage = $lang['fl_notback'];
-if (is_numeric($_POST['fleetid'])) {
-    $fleetid = intval($_POST['fleetid']);
+/**
+ * Description of ShowFleetBackPage
+ *
+ * @author author XNovaPT Team <xnovaptteam@gmail.com>
+ */
+class ShowFleetBackPage extends AbstractGamePage {
 
-    $FleetRow = doquery("SELECT * FROM {{table}} WHERE `fleet_id` = '" . $fleetid . "';", 'fleets', true);
-    $i = 0;
+    function __construct() {
+        parent::__construct();
+        $this->tplObj->compile_id = 'fleetback';
+    }
 
-    if ($FleetRow['fleet_owner'] == $user['id']) {
-        if ($FleetRow['fleet_mess'] == 0) {
-            if ($FleetRow['fleet_end_stay'] != 0) {
-                // Faut calculer le temps reel de retour
-                if ($FleetRow['fleet_start_time'] < time()) {
-                    // On a pas encore entamé le stationnement
-                    // Il faut calculer la parcelle de temps ecoulée depuis le lancement de la flotte
-                    $CurrentFlyingTime = time() - $FleetRow['start_time'];
-                } else {
-                    // On est deja en stationnement
-                    // Il faut donc directement calculer la durée d'un vol aller ou retour
-                    $CurrentFlyingTime = $FleetRow['fleet_start_time'] - $FleetRow['start_time'];
+    function show() {
+        global $user, $lang;
+        includeLang('fleet');
+
+        $BoxTitle = $lang['fl_error'];
+        $TxtColor = "red";
+        $BoxMessage = $lang['fl_notback'];
+        if (is_numeric($_POST['fleetid'])) {
+            $fleetid = intval($_POST['fleetid']);
+
+            $FleetRow = doquery("SELECT * FROM {{table}} WHERE `fleet_id` = '" . $fleetid . "';", 'fleets', true);
+            $i = 0;
+
+            if ($FleetRow['fleet_owner'] == $user['id']) {
+                if ($FleetRow['fleet_mess'] == 0) {
+                    if ($FleetRow['fleet_end_stay'] != 0) {
+                        // Faut calculer le temps reel de retour
+                        if ($FleetRow['fleet_start_time'] < time()) {
+                            // On a pas encore entamé le stationnement
+                            // Il faut calculer la parcelle de temps ecoulée depuis le lancement de la flotte
+                            $CurrentFlyingTime = time() - $FleetRow['start_time'];
+                        } else {
+                            // On est deja en stationnement
+                            // Il faut donc directement calculer la durée d'un vol aller ou retour
+                            $CurrentFlyingTime = $FleetRow['fleet_start_time'] - $FleetRow['start_time'];
+                        }
+                    } else {
+                        // C'est quoi le stationnement ??
+                        // On calcule sagement la parcelle de temps ecoulée depuis le depart
+                        $CurrentFlyingTime = time() - $FleetRow['start_time'];
+                    }
+                    // Allez houste au bout du compte y a la maison !! (E.T. phone home.............)
+                    $ReturnFlyingTime = $CurrentFlyingTime + time();
+
+                    $QryUpdateFleet = "UPDATE {{table}} SET ";
+                    $QryUpdateFleet .= "`fleet_start_time` = '" . (time() - 1) . "', ";
+                    $QryUpdateFleet .= "`fleet_end_stay` = '0', ";
+                    $QryUpdateFleet .= "`fleet_end_time` = '" . ($ReturnFlyingTime + 1) . "', ";
+                    $QryUpdateFleet .= "`fleet_target_owner` = '" . $user['id'] . "', ";
+                    $QryUpdateFleet .= "`fleet_mess` = '1' ";
+                    $QryUpdateFleet .= "WHERE ";
+                    $QryUpdateFleet .= "`fleet_id` = '" . $fleetid . "';";
+                    doquery($QryUpdateFleet, 'fleets');
+
+                    $BoxTitle = $lang['fl_sback'];
+                    $TxtColor = "lime";
+                    $BoxMessage = $lang['fl_isback'];
+                } elseif ($FleetRow['fleet_mess'] == 1) {
+                    $BoxMessage = $lang['fl_notback'];
                 }
             } else {
-                // C'est quoi le stationnement ??
-                // On calcule sagement la parcelle de temps ecoulée depuis le depart
-                $CurrentFlyingTime = time() - $FleetRow['start_time'];
+                $BoxMessage = $lang['fl_onlyyours'];
             }
-            // Allez houste au bout du compte y a la maison !! (E.T. phone home.............)
-            $ReturnFlyingTime = $CurrentFlyingTime + time();
-
-            $QryUpdateFleet = "UPDATE {{table}} SET ";
-            $QryUpdateFleet .= "`fleet_start_time` = '" . (time() - 1) . "', ";
-            $QryUpdateFleet .= "`fleet_end_stay` = '0', ";
-            $QryUpdateFleet .= "`fleet_end_time` = '" . ($ReturnFlyingTime + 1) . "', ";
-            $QryUpdateFleet .= "`fleet_target_owner` = '" . $user['id'] . "', ";
-            $QryUpdateFleet .= "`fleet_mess` = '1' ";
-            $QryUpdateFleet .= "WHERE ";
-            $QryUpdateFleet .= "`fleet_id` = '" . $fleetid . "';";
-            doquery($QryUpdateFleet, 'fleets');
-
-            $BoxTitle = $lang['fl_sback'];
-            $TxtColor = "lime";
-            $BoxMessage = $lang['fl_isback'];
-        } elseif ($FleetRow['fleet_mess'] == 1) {
-            $BoxMessage = $lang['fl_notback'];
         }
-    } else {
-        $BoxMessage = $lang['fl_onlyyours'];
-    }
-}
 
-message("<font color=\"" . $TxtColor . "\">" . $BoxMessage . "</font>", $BoxTitle, "fleet.php", 2);
+        message("<font color=\"" . $TxtColor . "\">" . $BoxMessage . "</font>", $BoxTitle, "fleet.php", 2);
 
 // -----------------------------------------------------------------------------------------------------------
 // History version
@@ -91,3 +100,6 @@ message("<font color=\"" . $TxtColor . "\">" . $BoxMessage . "</font>", $BoxTitl
 // 06.12.2007 - 08:41
 // Open Source
 // (c) by MoF
+    }
+
+}
