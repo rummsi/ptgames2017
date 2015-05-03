b95c2dcae8596d1ef39471471e5128a4be51f95c
rummsi <rummsi@fc0ce58d-76a3-41d0-bf4d-8f37abf19d28>
2015-05-03 18:54:50 +0000

    - Conversão do ShowFloten1Page.php para o Smarty.

--- branches/new_structure/Libraries/Pages/Game/ShowFloten1Page.php
+++ branches/new_structure/Libraries/Pages/Game/ShowFloten1Page.php
@@ -33,14 +33,12 @@ class ShowFloten1Page extends AbstractGamePage {
     function __construct() {
         parent::__construct();
         $this->tplObj->compile_id = 'floten1';
-        includeLang('fleet');
     }
 
     function show() {
         global $user, $planetrow, $lang, $resource, $pricelist, $reslist, $target_mission, $speedallsmin;
 
         includeLang('fleet');
-
         $speed = array(10 => 100,
             9 => 90,
             8 => 80,
@@ -52,12 +50,10 @@ class ShowFloten1Page extends AbstractGamePage {
             2 => 20,
             1 => 10,
         );
-
         $g = $_POST['galaxy'];
         $s = $_POST['system'];
         $p = $_POST['planet'];
         $t = $_POST['planet_type'];
-
         if (!$g) {
             $g = $planetrow['galaxy'];
         }
@@ -70,24 +66,23 @@ class ShowFloten1Page extends AbstractGamePage {
         if (!$t) {
             $t = $planetrow['planet_type'];
         }
-
         // Verifions si nous avons bien tout ce que nous voullons envoyer
         $FleetHiddenBlock = "";
         foreach ($reslist['fleet'] as $n => $i) {
-            if ($i > 200 && $i < 300 && $_POST["ship$i"] > "0") {
+            if ($i > 200 && $i < 300 && @$_POST["ship$i"] > "0") {
                 if ($_POST["ship$i"] > $planetrow[$resource[$i]]) {
                     $page .= $lang['fl_noenought'];
                     $speedalls[$i] = GetFleetMaxSpeed("", $i, $user);
                 } else {
                     $fleet['fleetarray'][$i] = $_POST["ship$i"];
                     // Tableau des vaisseaux avec leur nombre
-                    $fleet['fleetlist'] .= $i . "," . $_POST["ship$i"] . ";";
+                    @$fleet['fleetlist'] .= $i . "," . $_POST["ship$i"] . ";";
                     // Nombre total de vaisseaux
-                    $fleet['amount'] += $_POST["ship$i"];
+                    @$fleet['amount'] += $_POST["ship$i"];
                     // Tableau des vitesses
-                    $FleetHiddenBlock .= "<input type=\"hidden\" name=\"consumption" . $i . "\" value=\"" . GetShipConsumption($i, $user) . "\" />";
-                    $FleetHiddenBlock .= "<input type=\"hidden\" name=\"speed" . $i . "\"       value=\"" . GetFleetMaxSpeed("", $i, $user) . "\" />";
-                    $FleetHiddenBlock .= "<input type=\"hidden\" name=\"capacity" . $i . "\"    value=\"" . $pricelist[$i]['capacity'] . "\" />";
+                    $FleetHiddenBlock .= "<input type=\"hidden\" name=\"consumption" . $i . "\" value=\"" . GetShipConsumption($i, $user) . "\" />\n";
+                    $FleetHiddenBlock .= "<input type=\"hidden\" name=\"speed" . $i . "\"       value=\"" . GetFleetMaxSpeed("", $i, $user) . "\" />\n";
+                    $FleetHiddenBlock .= "<input type=\"hidden\" name=\"capacity" . $i . "\"    value=\"" . $pricelist[$i]['capacity'] . "\" />\n";
                     $FleetHiddenBlock .= "<input type=\"hidden\" name=\"ship" . $i . "\"        value=\"" . $_POST["ship$i"] . "\" />";
                     $speedalls[$i] = GetFleetMaxSpeed("", $i, $user);
                 }
@@ -98,83 +93,11 @@ class ShowFloten1Page extends AbstractGamePage {
         } else {
             $speedallsmin = min($speedalls);
         }
-        $page .= "<script type=\"text/javascript\" src=\"scripts/flotten.js\"></script>";
-        $page .= "<script type=\"text/javascript\">\n";
-        $page .= "function getStorageFaktor() {\n";
-        $page .= "	return 1\n";
-        $page .= "}\n";
-        $page .= "</script>\n";
-        $page .= "<form action=\"game.php?page=floten2\" method=\"post\">";
-        $page .= $FleetHiddenBlock;
-        $page .= "<input type=\"hidden\" name=\"speedallsmin\"   value=\"" . $speedallsmin . "\" />";
-        $page .= "<input type=\"hidden\" name=\"usedfleet\"      value=\"" . str_rot13(base64_encode(serialize($fleet['fleetarray']))) . "\" />";
-        $page .= "<input type=\"hidden\" name=\"thisgalaxy\"     value=\"" . $planetrow['galaxy'] . "\" />";
-        $page .= "<input type=\"hidden\" name=\"thissystem\"     value=\"" . $planetrow['system'] . "\" />";
-        $page .= "<input type=\"hidden\" name=\"thisplanet\"     value=\"" . $planetrow['planet'] . "\" />";
-        $page .= "<input type=\"hidden\" name=\"galaxyend\"      value=\"" . intval($_POST['galaxy']) . "\" />";
-        $page .= "<input type=\"hidden\" name=\"systemend\"      value=\"" . intval($_POST['system']) . "\" />";
-        $page .= "<input type=\"hidden\" name=\"planetend\"      value=\"" . intval($_POST['planet']) . "\" />";
-        $page .= "<input type=\"hidden\" name=\"speedfactor\"    value=\"" . GetGameSpeedFactor() . "\" />";
-        $page .= "<input type=\"hidden\" name=\"thisplanettype\" value=\"" . $planetrow['planet_type'] . "\" />";
-        $page .= "<input type=\"hidden\" name=\"thisresource1\"  value=\"" . floor($planetrow['metal']) . "\" />";
-        $page .= "<input type=\"hidden\" name=\"thisresource2\"  value=\"" . floor($planetrow['crystal']) . "\" />";
-        $page .= "<input type=\"hidden\" name=\"thisresource3\"  value=\"" . floor($planetrow['deuterium']) . "\" />";
-        $page .= "<br><div><center>";
-        $page .= "<table width=\"519\" border=\"0\" cellpadding=\"0\" cellspacing=\"1\">";
-        $page .= "<tr height=\"20\">";
-        $page .= "<td colspan=\"2\" class=\"c\">" . $lang['fl_floten1_ttl'] . "</td>";
-        $page .= "</tr>";
-        $page .= "<tr height=\"20\">";
-        $page .= "<th width=\"50%\">" . $lang['fl_dest'] . "</th>";
-        $page .= "<th>";
-        $page .= "<input name=\"galaxy\" size=\"3\" maxlength=\"2\" onChange=\"shortInfo()\" onKeyUp=\"shortInfo()\" value=\"" . $g . "\" />";
-        $page .= "<input name=\"system\" size=\"3\" maxlength=\"3\" onChange=\"shortInfo()\" onKeyUp=\"shortInfo()\" value=\"" . $s . "\" />";
-        $page .= "<input name=\"planet\" size=\"3\" maxlength=\"2\" onChange=\"shortInfo()\" onKeyUp=\"shortInfo()\" value=\"" . $p . "\" />";
-        $page .= "<select name=\"planettype\" onChange=\"shortInfo()\" onKeyUp=\"shortInfo()\">";
-        $page .= "<option value=\"1\"" . (($t == 1) ? " SELECTED" : "" ) . ">" . $lang['fl_planet'] . " </option>";
-        $page .= "<option value=\"2\"" . (($t == 2) ? " SELECTED" : "" ) . ">" . $lang['fl_ruins'] . " </option>";
-        $page .= "<option value=\"3\"" . (($t == 3) ? " SELECTED" : "" ) . ">" . $lang['fl_moon'] . " </option>";
-        $page .= "</select>";
-        $page .= "</th>";
-        $page .= "</tr>";
-        $page .= "<tr height=\"20\">";
-        $page .= "<th>" . $lang['fl_speed'] . "</th>";
-        $page .= "<th>";
-        $page .= "<select name=\"speed\" onChange=\"shortInfo()\" onKeyUp=\"shortInfo()\">";
+        $speed_ab="";
         foreach ($speed as $a => $b) {
-            $page .= "<option value=\"" . $a . "\">" . $b . "</option>";
+            $speed_ab .= "<option value=\"" . $a . "\">" . $b . "</option>\n";
         }
-        $page .= "</select> %";
-        $page .= "</th>";
-        $page .= "</tr>";
-        $page .= "<tr height=\"20\">";
-        $page .= "<th>" . $lang['fl_dist'] . "</th>";
-        $page .= "<th><div id=\"distance\">-</div></th>";
-        $page .= "</tr><tr height=\"20\">";
-        $page .= "<th>" . $lang['fl_fltime'] . "</th>";
-        $page .= "<th><div id=\"duration\">-</div></th>";
-        $page .= "</tr><tr height=\"20\">";
-        /* A faire assez rapidement (faut juste savoir comment)
-          $page .= "<th>". $lang['fl_time_go'] ."</th>";
-          $page .= "<th><font color=\"lime\"><div id=\"llegada1\"><font>". gmdate("H:i:s") ."</font></div></font></th>";
-          $page .= "</tr><tr height=\"20\">";
-          $page .= "<th>". $lang['fl_time_back'] ."</th>";
-          $page .= "<th><font color=\"lime\"><div id=\"llegada2\"><font>". gmdate("H:i:s") ."</font></div></font></th>";
-          $page .= "</tr><tr height=\"20\">";
-         */
-        $page .= "<th>" . $lang['fl_deute_need'] . "</th>";
-        $page .= "<th><div id=\"consumption\">-</div></th>";
-        $page .= "</tr><tr height=\"20\">";
-        $page .= "<th>" . $lang['fl_speed_max'] . "</th>";
-        $page .= "<th><div id=\"maxspeed\">-</div></th>";
-        $page .= "</tr><tr height=\"20\">";
-        $page .= "<th>" . $lang['fl_max_load'] . "</th>";
-        $page .= "<th><div id=\"storage\">-</div></th>";
-        $page .= "</tr>";
-        // Gestion des raccourcis sur la galaxie
-        $page .= "<tr height=\"20\">";
-        $page .= "<td colspan=\"2\" class=\"c\">" . $lang['fl_shortcut'] . " <a href=\"game.php?page=fleetshortcut\">" . $lang['fl_shortlnk'] . "</a></td>";
-        $page .= "</tr>";
+        $fleet_short = "";
         if ($user['fleet_shortcut']) {
             $scarray = explode("\r\n", $user['fleet_shortcut']);
             $i = 0;
@@ -182,24 +105,24 @@ class ShowFloten1Page extends AbstractGamePage {
                 if ($b != "") {
                     $c = explode(',', $b);
                     if ($i == 0) {
-                        $page .= "<tr height=\"20\">";
+                        $fleet_short .= "<tr height=\"20\">";
                     }
-                    $page .= "<th><a href=\"javascript:setTarget(" . $c[1] . "," . $c[2] . "," . $c[3] . "," . $c[4] . "); shortInfo();\"";
-                    $page .= ">" . $c[0] . " " . $c[1] . ":" . $c[2] . ":" . $c[3] . " ";
+                    $fleet_short .= "<th><a href=\"javascript:setTarget(" . $c[1] . "," . $c[2] . "," . $c[3] . "," . $c[4] . "); shortInfo();\"";
+                    $fleet_short .= ">" . $c[0] . " " . $c[1] . ":" . $c[2] . ":" . $c[3] . " ";
                     // Signalisation du type de raccourci ...
                     // (P)lanete
                     // (D)ebris
                     // (L)une
                     if ($c[4] == 1) {
-                        $page .= $lang['fl_shrtcup1'];
+                        $fleet_short .= $lang['fl_shrtcup1'];
                     } elseif ($c[4] == 2) {
-                        $page .= $lang['fl_shrtcup2'];
+                        $fleet_short .= $lang['fl_shrtcup2'];
                     } elseif ($c[4] == 3) {
-                        $page .= $lang['fl_shrtcup3'];
+                        $fleet_short .= $lang['fl_shrtcup3'];
                     }
-                    $page .= "</a></th>";
+                    $fleet_short .= "</a></th>";
                     if ($i == 1) {
-                        $page .= "</tr>";
+                        $fleet_short .= "</tr>";
                     }
                     if ($i == 1) {
                         $i = 0;
@@ -209,30 +132,28 @@ class ShowFloten1Page extends AbstractGamePage {
                 }
             }
             if ($i == 1) {
-                $page .= "<th></th></tr>";
+                $fleet_short .= "<th></th></tr>";
             }
         } else {
-            $page .= "<tr height=\"20\">";
-            $page .= "<th colspan=\"2\">" . $lang['fl_noshortc'] . "</th>";
-            $page .= "</tr>";
+            $fleet_short .= "<tr height=\"20\">";
+            $fleet_short .= "<th colspan=\"2\">" . $lang['fl_noshortc'] . "</th>";
+            $fleet_short .= "</tr>";
         }
-        $page .= "<tr height=\"20\">";
-        $page .= "<td colspan=\"2\" class=\"c\">" . $lang['fl_myplanets'] . "</td>";
-        $page .= "</tr>";
         // Gestion des raccourcis vers ses propres colonies ou planetes
         $kolonien = SortUserPlanets($user);
         $currentplanet = doquery("SELECT * FROM {{table}} WHERE id = '" . $user['current_planet'] . "'", 'planets', true);
+        $kolonien_page = "";
         if (mysql_num_rows($kolonien) > 1) {
             $i = 0;
             $w = 0;
             $tr = true;
             while ($row = mysql_fetch_array($kolonien)) {
                 if ($w == 0 && $tr) {
-                    $page .= "<tr height=\"20\">";
+                    $kolonien_page .= "<tr height=\"20\">";
                     $tr = false;
                 }
                 if ($w == 2) {
-                    $page .= "</tr>";
+                    $kolonien_page .= "</tr>";
                     $w = 0;
                     $tr = true;
                 }
@@ -243,43 +164,66 @@ class ShowFloten1Page extends AbstractGamePage {
                         $currentplanet['system'] == $row['system'] &&
                         $currentplanet['planet'] == $row['planet'] &&
                         $currentplanet['planet_type'] == $row['planet_type']) {
-                        //$page .= '<th><a href="javascript:setTarget('.$row['galaxy'].','.$row['system'].','.$row['planet'].','.$row['planet_type'].'); shortInfo();">'.$row['name'].' '.$row['galaxy'].':'.$row['system'].':'.$row['planet'].'</a></th>';
+                    //$page .= '<th><a href="javascript:setTarget('.$row['galaxy'].','.$row['system'].','.$row['planet'].','.$row['planet_type'].'); shortInfo();">'.$row['name'].' '.$row['galaxy'].':'.$row['system'].':'.$row['planet'].'</a></th>';
                 } else {
-                    $page .= "<th><a href=\"javascript:setTarget(" . $row['galaxy'] . "," . $row['system'] . "," . $row['planet'] . "," . $row['planet_type'] . "); shortInfo();\">" . $row['name'] . " " . $row['galaxy'] . ":" . $row['system'] . ":" . $row['planet'] . "</a></th>";
+                    $kolonien_page .= "<th><a href=\"javascript:setTarget(" . $row['galaxy'] . "," . $row['system'] . "," . $row['planet'] . "," . $row['planet_type'] . "); shortInfo();\">" . $row['name'] . " " . $row['galaxy'] . ":" . $row['system'] . ":" . $row['planet'] . "</a></th>";
                     $w++;
                     $i++;
                 }
             }
             if ($i % 2 != 0) {
-                $page .= "<th>&nbsp;</th></tr>";
+                $kolonien_page .= "<th>&nbsp;</th></tr>";
             } elseif ($w == 2) {
-                $page .= "</tr>";
+                $kolonien_page .= "</tr>";
             }
         } else {
-            $page .= "<th colspan=\"2\">" . $lang['fl_nocolonies'] . "</th>";
+            $kolonien_page .= "<th colspan=\"2\">" . $lang['fl_nocolonies'] . "</th>";
         }
-        $page .= "</tr>";
-        $page .= "<tr height=\"20\">";
-        $page .= "<td colspan=\"2\" class=\"c\">" . $lang['fl_grattack'] . "</td>";
-        $page .= "</tr>";
-        $page .= "<tr height=\"20\">";
-        $page .= "<th colspan=\"2\">-</th>";
-        $page .= "</tr>";
-        $page .= "<tr height=\"20\">";
-        $page .= "<th colspan=\"2\"><input type=\"submit\" value=\"" . $lang['fl_continue'] . "\" /></th>";
-        $page .= "</tr>";
-        $page .= "</table>";
-        $page .= "</div></center>";
-        $page .= "<input type=\"hidden\" name=\"maxepedition\" value=\"" . $_POST['maxepedition'] . "\" />";
-        $page .= "<input type=\"hidden\" name=\"curepedition\" value=\"" . $_POST['curepedition'] . "\" />";
-        $page .= "<input type=\"hidden\" name=\"target_mission\" value=\"" . $target_mission . "\" />";
-        $page .= "</form>";
-        $page .= "<script>javascript:shortInfo(); </script>";
 
-        Game::display($page, $lang['fl_title']);
-
-        // Updated by Chlorel. 16 Jan 2008 (String extraction, bug corrections, code uniformisation
-        // Created by Perberos. All rights reversed (C) 2006
+        $this->tplObj->assign(array(
+            'title' => $lang['fl_title'],
+            'FleetHiddenBlock' => $FleetHiddenBlock,
+            'speedallsmin' => $speedallsmin,
+            'f_fleetarray' => str_rot13(base64_encode(serialize($fleet['fleetarray']))),
+            'galaxy' => $planetrow['galaxy'],
+            'system' => $planetrow['system'],
+            'planet' => $planetrow['planet'],
+            'Pgalaxy' => intval(filter_input(INPUT_POST, 'galaxy')),
+            'Psystem' => intval(filter_input(INPUT_POST, 'system')),
+            'Pplanet' => intval(filter_input(INPUT_POST, 'planet')),
+            'GetGameSpeedFactor' => GetGameSpeedFactor(),
+            'planet_type' => $planetrow['planet_type'],
+            'fmetal' => floor($planetrow['metal']),
+            'fcrystal' => floor($planetrow['crystal']),
+            'fdeuterium' => floor($planetrow['deuterium']),
+            'fl_floten1_ttl' => $lang['fl_floten1_ttl'],
+            'fl_dest' => $lang['fl_dest'],
+            'g' => $g,
+            's' => $s,
+            'p' => $p,
+            't' => $t,
+            'fl_planet' => $lang['fl_planet'],
+            'fl_ruins' => $lang['fl_ruins'],
+            'fl_moon' => $lang['fl_moon'],
+            'fl_speed' => $lang['fl_speed'],
+            'speed_ab' => $speed_ab,
+            'fl_dist' => $lang['fl_dist'],
+            'fl_fltime' => $lang['fl_fltime'],
+            'fl_deute_need' => $lang['fl_deute_need'],
+            'fl_speed_max' => $lang['fl_speed_max'],
+            'fl_max_load' => $lang['fl_max_load'],
+            'fl_shortcut' => $lang['fl_shortcut'],
+            'fl_shortlnk' => $lang['fl_shortlnk'],
+            'fleet_short' => $fleet_short,
+            'fl_myplanets' => $lang['fl_myplanets'],
+            'kolonien_page' => $kolonien_page,
+            'fl_grattack' => $lang['fl_grattack'],
+            'fl_continue' => $lang['fl_continue'],
+            'Pmaxepedition' => filter_input(INPUT_POST, 'maxepedition'),
+            'Pcurepedition' => filter_input(INPUT_POST, 'curepedition'),
+            'target_mission' => $target_mission,
+        ));
+        $this->render('Fleet/floten1.tpl');
     }
 
 }
