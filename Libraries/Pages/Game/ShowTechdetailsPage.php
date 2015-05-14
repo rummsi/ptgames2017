6ff92ab6df5b7eb203b95fa57611a99259d29e3f
rummsi <rummsi@gmail.com>
2015-05-14 11:51:48 +0000

    - Criada a class ShowTechdetailsPage. - Eliminação da função referente à cache.

--- Libraries/Pages/Game/ShowTechdetailsPage.php
+++ Libraries/Pages/Game/ShowTechdetailsPage.php
@@ -1,50 +1,63 @@
 <?php
 
-/**
- * This file is part of XNova:Legacies
- *
- * @license http://www.gnu.org/licenses/gpl-3.0.txt
- * @see http://www.xnova-ng.org/
- *
- * Copyright (c) 2009-Present, XNova Support Team <http://www.xnova-ng.org>
- * All rights reserved.
- *
+/*
+ * XNovaPT
+ * Copyright (C) 2012
+ * 
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
  * the Free Software Foundation, either version 3 of the License, or
  * (at your option) any later version.
- *
+ * 
  * This program is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License
- * along with this program.  If not, see <http://www.gnu.org/licenses/>.
- *
- *                                --> NOTICE <--
- *  This file is part of the core development branch, changing its contents will
- * make you unable to use the automatic updates manager. Please refer to the
- * documentation for further information about customizing XNova.
- *
+ * 
+ * You should read the GNU General Public License, see <http://www.gnu.org/licenses/>.
+ * 
+ * XNovaPT
+ * @author XNovaPT Team <xnovaptteam@gmail.com>
+ * @ShowTechdetailsPage.php
+ * @license http://www.gnu.org/licenses/gpl.html GNU GPLv3 License
+ * @version 0.01  14/mai/2015 12:47:44
  */
-$Id = $_GET['techid'];
-$PageTPL = gettemplate('techtree_details');
-$RowsTPL = gettemplate('techtree_details_rows');
 
-$parse = $lang;
-$parse['te_dt_id'] = $Id;
-$parse['te_dt_name'] = $lang['tech'][$Id];
-$Liste = "";
+/**
+ * Description of ShowTechdetailsPage
+ *
+ * @author author XNovaPT Team <xnovaptteam@gmail.com>
+ */
+class ShowTechdetailsPage extends AbstractGamePage {
 
-if ($Id == 12) {
-    $Liste .= "<tr><th>" . $lang['tech']['31'] . " (" . $lang['level'] . " 1)</th></tr>";
-    $Liste .= "<tr><td class=\"c\">2</td><tr>";
-    $Liste .= "<tr><th>" . $lang['tech']['3'] . " (" . $lang['level'] . " 5)</th></tr>";
-    $Liste .= "<tr><th>" . $lang['tech']['113'] . " (" . $lang['level'] . " 3) <a href=\"techtreedetails.php?tech=113\">[i]</a></th></tr>";
+    function __construct() {
+        parent::__construct();
+        $this->tplObj->compile_id = 'techdetails';
+    }
+
+    function show() {
+        global $lang;
+
+        $Id = $_GET['techid'];
+        $PageTPL = gettemplate('techtree_details');
+        $RowsTPL = gettemplate('techtree_details_rows');
+
+        $parse = $lang;
+        $parse['te_dt_id'] = $Id;
+        $parse['te_dt_name'] = $lang['tech'][$Id];
+        $Liste = "";
+
+        if ($Id == 12) {
+            $Liste .= "<tr><th>" . $lang['tech']['31'] . " (" . $lang['level'] . " 1)</th></tr>";
+            $Liste .= "<tr><td class=\"c\">2</td><tr>";
+            $Liste .= "<tr><th>" . $lang['tech']['3'] . " (" . $lang['level'] . " 5)</th></tr>";
+            $Liste .= "<tr><th>" . $lang['tech']['113'] . " (" . $lang['level'] . " 3) <a href=\"techtreedetails.php?tech=113\">[i]</a></th></tr>";
+        }
+
+        $parse['Liste'] = $Liste;
+        $page = parsetemplate($PageTPL, $parse);
+
+        Game::display($page, $lang['Tech'], false, '', false);
+    }
+
 }
-
-$parse['Liste'] = $Liste;
-$page = parsetemplate($PageTPL, $parse);
-
-Game::display($page, $lang['Tech'], false, '', false);
