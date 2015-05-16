4dee97038ebf99d32965aa38d2e97f8106846a83
rummsi <rummsi@fc0ce58d-76a3-41d0-bf4d-8f37abf19d28>
2015-05-16 20:46:23 +0000

    - Criada a class ShowBannedPage.

--- branches/new_structure/Libraries/Pages/Game/ShowBannedPage.php
+++ branches/new_structure/Libraries/Pages/Game/ShowBannedPage.php
@@ -1,56 +1,63 @@
 <?php
 
-/**
- * This file is part of XNova:Legacies
- *
- * @license http://www.gnu.org/licenses/gpl-3.0.txt
- * @see http://www.xnova-ng.org/
- *
- * Copyright (c) 2009-Present, XNova Support Team <http://www.xnova-ng.org>
- * All rights reserved.
- *
+/*
+ * XNovaPT
+ * Copyright (C) 2012
+ * 
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
  * the Free Software Foundation, either version 3 of the License, or
  * (at your option) any later version.
- *
+ * 
  * This program is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License
- * along with this program.  If not, see <http://www.gnu.org/licenses/>.
- *
- *                                --> NOTICE <--
- *  This file is part of the core development branch, changing its contents will
- * make you unable to use the automatic updates manager. Please refer to the
- * documentation for further information about customizing XNova.
- *
+ * 
+ * You should read the GNU General Public License, see <http://www.gnu.org/licenses/>.
+ * 
+ * XNovaPT
+ * @author XNovaPT Team <xnovaptteam@gmail.com>
+ * @ShowBannedPage.php
+ * @license http://www.gnu.org/licenses/gpl.html GNU GPLv3 License
+ * @version 0.01  16/mai/2015 21:45:11
  */
-includeLang('banned');
 
-$parse = $lang;
-$parse['dpath'] = $dpath;
-$parse['mf'] = $mf;
+/**
+ * Description of ShowBannedPage
+ *
+ * @author author XNovaPT Team <xnovaptteam@gmail.com>
+ */
+class ShowBannedPage extends AbstractGamePage {
 
-$query = doquery("SELECT * FROM {{table}} ORDER BY `id`;", 'banned');
-$i = 0;
-while ($u = mysql_fetch_array($query)) {
-    $parse['banned'] .=
-            "<tr><td class=b><center><b>" . $u[1] . "</center></td></b>" .
-            "<td class=b><center><b>" . $u[2] . "</center></b></td>" .
-            "<td class=b><center><b>" . date("d/m/Y G:i:s", $u[4]) . "</center></b></td>" .
-            "<td class=b><center><b>" . date("d/m/Y G:i:s", $u[5]) . "</center></b></td>" .
-            "<td class=b><center><b>" . $u[6] . "</center></b></td></tr>";
-    $i++;
+    function __construct() {
+        parent::__construct();
+        $this->tplObj->compile_id = 'banned';
+    }
+
+    function show() {
+        global $lang;
+
+        includeLang('banned');
+        $parse = $lang;
+        $query = doquery("SELECT * FROM {{table}} ORDER BY `id`;", 'banned');
+        $i = 0;
+        while ($u = mysql_fetch_array($query)) {
+            $parse['banned'] .=
+                    "<tr><td class=b><center><b>" . $u[1] . "</center></td></b>" .
+                    "<td class=b><center><b>" . $u[2] . "</center></b></td>" .
+                    "<td class=b><center><b>" . date("d/m/Y G:i:s", $u[4]) . "</center></b></td>" .
+                    "<td class=b><center><b>" . date("d/m/Y G:i:s", $u[5]) . "</center></b></td>" .
+                    "<td class=b><center><b>" . $u[6] . "</center></b></td></tr>";
+            $i++;
+        }
+        if ($i == "0") {
+            $parse['banned'] .= "<tr><th class=b colspan=6>Il n'y a pas de joueurs bannis</th></tr>";
+        } else {
+            $parse['banned'] .= "<tr><th class=b colspan=6>Il y a {$i} joueurs bannis</th></tr>";
+        }
+        Game::display(parsetemplate(gettemplate('banned_body'), $parse), 'Banned', true);
+        // Created by e-Zobar (XNova Team). All rights reversed (C) 2008
+    }
+
 }
-
-if ($i == "0")
-    $parse['banned'] .= "<tr><th class=b colspan=6>Il n'y a pas de joueurs bannis</th></tr>";
-else
-    $parse['banned'] .= "<tr><th class=b colspan=6>Il y a {$i} joueurs bannis</th></tr>";
-
-Game::display(parsetemplate(gettemplate('banned_body'), $parse), 'Banned', true);
-
-// Created by e-Zobar (XNova Team). All rights reversed (C) 2008
