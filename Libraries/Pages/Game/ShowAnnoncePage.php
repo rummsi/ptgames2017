0bff6145fb5336bab64f6d2cc4eac261039eed64
rummsi <rummsi@gmail.com>
2015-05-17 21:09:41 +0000

    - Conversão do ShowAnnoncePage.php para o Smarty.

--- Libraries/Pages/Game/ShowAnnoncePage.php
+++ Libraries/Pages/Game/ShowAnnoncePage.php
@@ -36,98 +36,62 @@ class ShowAnnoncePage extends AbstractGamePage {
     }
 
     function show() {
-        global $user, $lang, $dpath;
-        
+        global $user;
+
         $users = doquery("SELECT * FROM {{table}} WHERE id='" . $user['id'] . "';", 'users');
         $annonce = doquery("SELECT * FROM {{table}} ", 'annonce');
-        $action = $_GET['action'];
-
+        $action = HTTP::_GP('action', '');
         if ($action == 5) {
             $metalvendre = $_POST['metalvendre'];
             $cristalvendre = $_POST['cristalvendre'];
             $deutvendre = $_POST['deutvendre'];
-
             $metalsouhait = $_POST['metalsouhait'];
             $cristalsouhait = $_POST['cristalsouhait'];
             $deutsouhait = $_POST['deutsouhait'];
-
             while ($v_annonce = mysql_fetch_array($users)) {
                 $user = $v_annonce['username'];
                 $galaxie = $v_annonce['galaxy'];
                 $systeme = $v_annonce['system'];
             }
+            doquery("INSERT INTO {{table}} SET user='{$user}', galaxie='{$galaxie}', systeme='{$systeme}', metala='{$metalvendre}', cristala='{$cristalvendre}', deuta='{$deutvendre}', metals='{$metalsouhait}', cristals='{$cristalsouhait}', deuts='{$deutsouhait}'", "annonce");
 
-            doquery("INSERT INTO {{table}} SET
-user='{$user}',
-galaxie='{$galaxie}',
-systeme='{$systeme}',
-metala='{$metalvendre}',
-cristala='{$cristalvendre}',
-deuta='{$deutvendre}',
-metals='{$metalsouhait}',
-cristals='{$cristalsouhait}',
-deuts='{$deutsouhait}'", "annonce");
+            $this->tplObj->assign(array(
+                'title' => '',
+            ));
 
-            $page2 .= <<<HTML
-<center>
-<br>
-<p>Votre Annonce a bien &eacute;t&eacute; enregistr&eacute;e !</p>
-<br><p><a href="game.php?page=annonce">Retour aux annonces</a></p>
+            $this->render('annonce_ok.tpl');
+        } elseif ($action == 2) {
 
-HTML;
+            $this->tplObj->assign(array(
+                'title' => '',
+            ));
 
-            Game::display($page2);
-        }
-
-        if ($action != 5) {
+            $this->render('annonce2_body.tpl');
+        } else {
             $annonce = doquery("SELECT * FROM {{table}} ORDER BY `id` DESC ", "annonce");
-
-            $page2 = "<HTML>
-<center>
-<br>
-<table width=\"600\">
-<td class=\"c\" colspan=\"10\"><font color=\"#FFFFFF\">Petites Annonces</font></td></tr>
-<tr><th colspan=\"3\">Infos de livraison</th><th colspan=\"3\">Ressources &agrave; vendre</th><th colspan=\"3\">Ressources souhait&eacute;es</th><th>Action</th></tr>
-<tr><th>Vendeur</th><th>Galaxie</th><th>Syst&egrave;me</th><th>M&eacute;tal</th><th>Cristal</th><th>Deuterium</th><th>M&eacute;tal</th><th>Cristal</th><th>Deuterium</th><th>Delet</th></tr>
-
-
-
-
-";
+            $annonce_table = "";
             while ($b = mysql_fetch_array($annonce)) {
-                $page2 .= '<tr><th> ';
-                $page2 .= $b["user"];
-                $page2 .= '</th><th>';
-                $page2 .= $b["galaxie"];
-                $page2 .= '</th><th>';
-                $page2 .= $b["systeme"];
-                $page2 .= '</th><th>';
-                $page2 .= $b["metala"];
-                $page2 .= '</th><th>';
-                $page2 .= $b["gcristala"];
-                $page2 .= '</th><th>';
-                $page2 .= $b["deuta"];
-                $page2 .= '</th><th>';
-                $page2 .= $b["metals"];
-                $page2 .= '</th><th>';
-                $page2 .= $b["cristals"];
-                $page2 .= '</th><th>';
-                $page2 .= $b["deuts"];
-                $page2 .= '</th><th>';
-                $page2 .= "</th></tr>";
+                $this->tplObj->assign(array(
+                    'b_user' => $b["user"],
+                    'b_galaxie' => $b["galaxie"],
+                    'b_systeme' => $b["systeme"],
+                    'b_metala' => @$b["metala"],
+                    'b_gcristala' => @$b["gcristala"],
+                    'b_deuta' => @$b["deuta"],
+                    'b_metals' => @$b["metals"],
+                    'b_cristals' => @$b["cristals"],
+                    'b_deuts' => @$b["deuts"],
+                ));
+                $annonce_table .= $this->tplObj->fetch('annonce_table.tpl');
             }
 
-            $page2 .= "
-<tr><th colspan=\"10\" align=\"center\"><a href=\"game.php?page=annonce2&action=2\">Ajouter une Annonce</a></th></tr>
-</td>
-</table>
-</HTML>";
+            $this->tplObj->assign(array(
+                'title' => '',
+                'annonce_table' => $annonce_table,
+            ));
 
-            Game::display($page2);
+            $this->render('annonce_body.tpl');
         }
-
-// Créer par Tom1991 Copyright 2008
-// Merci au site Spacon pour m'avoir donner l'inspiration
     }
 
 }
