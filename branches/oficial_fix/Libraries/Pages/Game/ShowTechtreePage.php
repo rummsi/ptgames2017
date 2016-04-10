8e1ca46eebeaab3e2d9246e019df1aaf51b3afc0
rummsi <rummsi@fc0ce58d-76a3-41d0-bf4d-8f37abf19d28>
2016-04-10 20:38:33 +0000

    NEW: página de tecnologias.

---
+++ branches/oficial_fix/Libraries/Pages/Game/ShowTechtreePage.php
@@ -0,0 +1,81 @@
+<?php
+
+/*
+ * PTGamesPT
+ * Copyright (C) 2012
+ * 
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should read the GNU General Public License, see <http://www.gnu.org/licenses/>.
+ * 
+ * PTGamesPT
+ * @author XNovaPT Team <xnovaptteam@gmail.com>
+ * @ShowTechtreePage.php
+ * @license http://www.gnu.org/licenses/gpl.html GNU GPLv3 License
+ * @version 0.01  10/abr/2016 21:29:45
+ */
+
+/**
+ * Description of ShowTechtreePage
+ *
+ * @author author XNovaPT Team <xnovaptteam@gmail.com>
+ */
+class ShowTechtreePage extends AbstractGamePage {
+
+    function __construct() {
+        parent::__construct();
+        $this->tplObj->compile_id = 'techtree';
+    }
+
+    function show() {
+        global $lang, $resource, $requirements, $planetrow, $user;
+
+        $HeadTpl = gettemplate('techtree_head');
+        $RowTpl = gettemplate('techtree_row');
+        foreach ($lang['tech'] as $Element => $ElementName) {
+            $parse = array();
+            $parse['tt_name'] = $ElementName;
+            if (!isset($resource[$Element])) {
+                $parse['Requirements'] = $lang['Requirements'];
+                $page .= parsetemplate($HeadTpl, $parse);
+            } else {
+                if (isset($requirements[$Element])) {
+                    $parse['required_list'] = "";
+                    foreach ($requirements[$Element] as $ResClass => $Level) {
+                        if (isset($user[$resource[$ResClass]]) &&
+                                $user[$resource[$ResClass]] >= $Level) {
+                            $parse['required_list'] .= "<font color=\"#00ff00\">";
+                        } elseif (isset($planetrow[$resource[$ResClass]]) &&
+                                $planetrow[$resource[$ResClass]] >= $Level) {
+                            $parse['required_list'] .= "<font color=\"#00ff00\">";
+                        } else {
+                            $parse['required_list'] .= "<font color=\"#ff0000\">";
+                        }
+                        $parse['required_list'] .= $lang['tech'][$ResClass] . " (" . $lang['level'] . " " . $Level . ")";
+                        $parse['required_list'] .= "</font><br>";
+                    }
+                    $parse['tt_detail'] = "<a href=\"techdetails.php?techid=" . $Element . "\">" . $lang['treeinfo'] . "</a>";
+                } else {
+                    $parse['required_list'] = "";
+                    $parse['tt_detail'] = "";
+                }
+                $parse['tt_info'] = $Element;
+                $page .= parsetemplate($RowTpl, $parse);
+            }
+        }
+
+        $parse['techtree_list'] = $page;
+        $page = parsetemplate(gettemplate('techtree_body'), $parse);
+
+        display($page, $lang['Tech']);
+    }
+
+}
