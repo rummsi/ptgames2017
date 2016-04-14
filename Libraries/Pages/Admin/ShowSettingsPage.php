ea0220aa81a4fc053dc7e371e4335d0adf3c4384
rummsi <rummsi@gmail.com>
2016-04-14 23:52:03 +0000

    FIX: página de configurações de administrador.

--- Libraries/Pages/Admin/ShowSettingsPage.php
+++ Libraries/Pages/Admin/ShowSettingsPage.php
@@ -32,7 +32,7 @@ class ShowSettingsPage extends AbstractAdminPage {
 
     function __construct() {
         parent::__construct();
-        $this->tplObj->compile_id = 'changelog';
+        $this->tplObj->compile_id = 'settings';
     }
 
     function show() {
