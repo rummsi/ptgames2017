fb476bda393e23e0f34ab634b9014935fa2f00e7
rummsi <rummsi@gmail.com>
2016-04-16 13:54:43 +0000

    ADD: página de frotas em voo de administrador.

---
+++ Libraries/Pages/Admin/ShowShowFlyingFleetsPage.php
@@ -0,0 +1,55 @@
+<?php
+
+/*
+ * PTGamesPT
+ * Copyright (C) 2012
+ * 
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should read the GNU General Public License, see <http://www.gnu.org/licenses/>.
+ * 
+ * PTGamesPT
+ * @author XNovaPT Team <xnovaptteam@gmail.com>
+ * @ShowShowFlyingFleetsPage.php
+ * @license http://www.gnu.org/licenses/gpl.html GNU GPLv3 License
+ * @version 0.01  16/abr/2016 14:52:21
+ */
+
+/**
+ * Description of ShowShowFlyingFleetsPage
+ *
+ * @author author XNovaPT Team <xnovaptteam@gmail.com>
+ */
+class ShowShowFlyingFleetsPage extends AbstractAdminPage {
+
+    function __construct() {
+        parent::__construct();
+        $this->tplObj->compile_id = 'changelog';
+    }
+
+    function show() {
+        global $lang, $user;
+
+        if (in_array($user['authlevel'], array(LEVEL_ADMIN, LEVEL_OPERATOR, LEVEL_MODERATOR))) {
+            includeLang('admin/fleets');
+            $PageTPL = gettemplate('admin/fleet_body');
+
+            $parse = $lang;
+            $parse['flt_table'] = BuildFlyingFleetTable();
+
+            $page = parsetemplate($PageTPL, $parse);
+            display($page, $lang['flt_title'], false, '', true);
+        } else {
+            AdminMessage($lang['sys_noalloaw'], $lang['sys_noaccess']);
+        }
+    }
+
+}
