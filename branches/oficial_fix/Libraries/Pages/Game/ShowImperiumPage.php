2692ed4441bd211c1034f593a1f2fe0a2bb80009
rummsi <rummsi@fc0ce58d-76a3-41d0-bf4d-8f37abf19d28>
2016-06-12 16:46:39 +0000

    SMARTY: ShowImperiumPage.

--- branches/oficial_fix/Libraries/Pages/Game/ShowImperiumPage.php
+++ branches/oficial_fix/Libraries/Pages/Game/ShowImperiumPage.php
@@ -37,7 +37,7 @@ class ShowImperiumPage extends AbstractGamePage {
     }
 
     function show() {
-        global $lang, $user, $resource, $reslist, $dpath;
+        global $lang, $user, $resource, $reslist;
 
         define('NO_MENU', true);
 
@@ -57,83 +57,22 @@ class ShowImperiumPage extends AbstractGamePage {
         $planetsrow = doquery($QryPlanets, 'planets');
 
         $planet = array();
-        $parse = $lang;
 
         while ($p = mysqli_fetch_array($planetsrow)) {
             $planet[] = $p;
         }
 
-        $parse['mount'] = count($planet) + 1;
-// primera tabla, con las imagenes y coordenadas
-        $row = gettemplate('imperium_row');
-        $row2 = gettemplate('imperium_row2');
-
-        foreach ($planet as $p) {
-            PlanetResourceUpdate($user, $p, time());
-
-            // {file_images}
-            $data['text'] = '<a href="game.php?page=overview&cp=' . $p['id'] . '&amp;re=0"><img src="' . $dpath . 'planeten/small/s_' . $p['image'] . '.jpg" border="0" height="71" width="75"></a>';
-            $parse['file_images'] .= parsetemplate($row, $data);
-            // {file_names}
-            $data['text'] = $p['name'];
-            $parse['file_names'] .= parsetemplate($row2, $data);
-            // {file_coordinates}
-            $data['text'] = "[<a href=\"game.php?page=galaxy&type=3&galaxy={$p['galaxy']}&system={$p['system']}\">{$p['galaxy']}:{$p['system']}:{$p['planet']}</a>]";
-            $parse['file_coordinates'] .= parsetemplate($row2, $data);
-            // {file_fields}
-            $data['text'] = $p['field_current'] . '/' . $p['field_max'];
-            $parse['file_fields'] .= parsetemplate($row2, $data);
-            // {file_metal}
-            $data['text'] = '<a href="resources.php?cp=' . $p['id'] . '&amp;re=0&amp;planettype=' . $p['planet_type'] . '">' . pretty_number($p['metal']) . '</a> / ' . pretty_number($p['metal_perhour']);
-            $parse['file_metal'] .= parsetemplate($row2, $data);
-            // {file_crystal}
-            $data['text'] = '<a href="resources.php?cp=' . $p['id'] . '&amp;re=0&amp;planettype=' . $p['planet_type'] . '">' . pretty_number($p['crystal']) . '</a> / ' . pretty_number($p['crystal_perhour']);
-            $parse['file_crystal'] .= parsetemplate($row2, $data);
-            // {file_deuterium}
-            $data['text'] = '<a href="resources.php?cp=' . $p['id'] . '&amp;re=0&amp;planettype=' . $p['planet_type'] . '">' . pretty_number($p['deuterium']) . '</a> / ' . pretty_number($p['deuterium_perhour']);
-            $parse['file_deuterium'] .= parsetemplate($row2, $data);
-            // {file_energy}
-            $data['text'] = pretty_number($p['energy_max'] - $p['energy_used']) . ' / ' . pretty_number($p['energy_max']);
-            $parse['file_energy'] .= parsetemplate($row2, $data);
-
-            foreach ($resource as $i => $res) {
-                if (in_array($i, $reslist['build']))
-                    $data['text'] = ($p[$resource[$i]] == 0) ? '-' : "<a href=\"game.php?page=buildings&cp={$p['id']}&amp;re=0&amp;planettype={$p['planet_type']}\">{$p[$resource[$i]]}</a>";
-                elseif (in_array($i, $reslist['tech']))
-                    $data['text'] = ($user[$resource[$i]] == 0) ? '-' : "<a href=\"game.php?page=research&cp={$p['id']}&amp;re=0&amp;planettype={$p['planet_type']}\">{$user[$resource[$i]]}</a>";
-                elseif (in_array($i, $reslist['fleet']))
-                    $data['text'] = ($p[$resource[$i]] == 0) ? '-' : "<a href=\"game.php?page=shipyard&cp={$p['id']}&amp;re=0&amp;planettype={$p['planet_type']}\">{$p[$resource[$i]]}</a>";
-                elseif (in_array($i, $reslist['defense']))
-                    $data['text'] = ($p[$resource[$i]] == 0) ? '-' : "<a href=\"game.php?page=defense&cp={$p['id']}&amp;re=0&amp;planettype={$p['planet_type']}\">{$p[$resource[$i]]}</a>";
-
-                $r[$i] .= parsetemplate($row2, $data);
-            }
-        }
-
-// {building_row}
-        foreach ($reslist['build'] as $a => $i) {
-            $data['text'] = $lang['tech'][$i];
-            $parse['building_row'] .= "<tr>" . parsetemplate($row2, $data) . $r[$i] . "</tr>";
-        }
-// {technology_row}
-        foreach ($reslist['tech'] as $a => $i) {
-            $data['text'] = $lang['tech'][$i];
-            $parse['technology_row'] .= "<tr>" . parsetemplate($row2, $data) . $r[$i] . "</tr>";
-        }
-// {fleet_row}
-        foreach ($reslist['fleet'] as $a => $i) {
-            $data['text'] = $lang['tech'][$i];
-            $parse['fleet_row'] .= "<tr>" . parsetemplate($row2, $data) . $r[$i] . "</tr>";
-        }
-// {defense_row}
-        foreach ($reslist['defense'] as $a => $i) {
-            $data['text'] = $lang['tech'][$i];
-            $parse['defense_row'] .= "<tr>" . parsetemplate($row2, $data) . $r[$i] . "</tr>";
-        }
-
-        $page .= parsetemplate(gettemplate('imperium_table'), $parse);
-
-        display($page, $lang['Imperium'], false);
+        $this->tplObj->assign(array(
+            'title' => $lang['Imperium'],
+            'lang' => $lang,
+            'mount' => count($planet) + 1,
+            'imperium_vision' => $lang['imperium_vision'],
+            'planet' => $planet,
+            'reslist' => $reslist,
+            'resource' => $resource,
+            'user' => $user,
+        ));
+        $this->render('imperium_table.tpl');
     }
 
 }
