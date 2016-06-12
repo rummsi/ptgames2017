33c6279c289485eff00f0c6fcb859b8d778f3e7c
rummsi <rummsi@fc0ce58d-76a3-41d0-bf4d-8f37abf19d28>
2016-06-12 10:59:06 +0000

    SMARTY: ShowMessagesPage.

--- branches/oficial_fix/Libraries/Pages/Game/ShowMessagesPage.php
+++ branches/oficial_fix/Libraries/Pages/Game/ShowMessagesPage.php
@@ -36,7 +36,7 @@ class ShowMessagesPage extends AbstractGamePage {
     }
 
     function show() {
-        global $user, $lang, $messfields, $dpath;
+        global $user, $lang, $messfields, $dpath, $game_config;
 
         include(ROOT_PATH . 'includes/functions/BBcodeFunction.' . PHPEXT);
 
@@ -47,9 +47,9 @@ class ShowMessagesPage extends AbstractGamePage {
         includeLang('messages');
 
         $OwnerID = $_GET['id'];
-        $MessCategory = $_GET['messcat'];
+        $MessCategory = @$_GET['messcat'];
         $MessPageMode = (string) $_GET['type'];
-        $DeleteWhat = $_POST['deletemessages'];
+        $DeleteWhat = @$_POST['deletemessages'];
         if (isset($DeleteWhat)) {
             $MessPageMode = "delete";
         }
@@ -59,7 +59,6 @@ class ShowMessagesPage extends AbstractGamePage {
 
         $MessageType = array(0, 1, 2, 3, 4, 5, 15, 99, 100);
         $TitleColor = array(0 => '#FFFF00', 1 => '#FF6699', 2 => '#FF3300', 3 => '#FF9900', 4 => '#773399', 5 => '#009933', 15 => '#030070', 99 => '#007070', 100 => '#ABABAB');
-        $BackGndColor = array(0 => '#663366', 1 => '#336666', 2 => '#000099', 3 => '#666666', 4 => '#999999', 5 => '#999999', 15 => '#999999', 99 => '#999999', 100 => '#999999');
 
         for ($MessType = 0; $MessType < 101; $MessType++) {
             if (in_array($MessType, $MessageType)) {
@@ -81,18 +80,14 @@ class ShowMessagesPage extends AbstractGamePage {
                 if (!is_numeric($OwnerID)) {
                     message($lang['mess_no_ownerid'], $lang['mess_error']);
                 }
-
                 $OwnerRecord = doquery("SELECT * FROM {{table}} WHERE `id` = '" . strval($OwnerID) . "';", 'users', true);
-
                 if (!$OwnerRecord) {
                     message($lang['mess_no_owner'], $lang['mess_error']);
                 }
-
                 $OwnerHome = doquery("SELECT * FROM {{table}} WHERE `id_planet` = '" . $OwnerRecord["id_planet"] . "';", 'galaxy', true);
                 if (!$OwnerHome) {
                     message($lang['mess_no_ownerpl'], $lang['mess_error']);
                 }
-
                 if ($_POST) {
                     $error = 0;
                     if (!$_POST["subject"]) {
@@ -107,8 +102,7 @@ class ShowMessagesPage extends AbstractGamePage {
                         $page .= "<center><font color=#00FF00>" . $lang['mess_sended'] . "<br></font></center>";
 
                         $_POST['text'] = str_replace("'", '&#39;', $_POST['text']);
-//					$_POST['text'] = str_replace('\r\n', '<br />', $_POST['text']);
-
+                        //$_POST['text'] = str_replace('\r\n', '<br />', $_POST['text']);
                         $Owner = $OwnerID;
                         $Sender = $user['id'];
                         $From = $user['username'] . " [" . $user['galaxy'] . ":" . $user['system'] . ":" . $user['planet'] . "]";
@@ -123,21 +117,22 @@ class ShowMessagesPage extends AbstractGamePage {
                         $text = "";
                     }
                 }
-                $parse['Send_message'] = $lang['mess_pagetitle'];
-                $parse['Recipient'] = $lang['mess_recipient'];
-                $parse['Subject'] = $lang['mess_subject'];
-                $parse['Message'] = $lang['mess_message'];
-                $parse['characters'] = $lang['mess_characters'];
-                $parse['Envoyer'] = $lang['mess_envoyer'];
 
-                $parse['id'] = $OwnerID;
-                $parse['to'] = $OwnerRecord['username'] . " [" . $OwnerHome['galaxy'] . ":" . $OwnerHome['system'] . ":" . $OwnerHome['planet'] . "]";
-                $parse['subject'] = (!isset($subject)) ? $lang['mess_no_subject'] : $subject;
-                $parse['text'] = $text;
+                $this->tplObj->assign(array(
+                    'title' => $lang['mess_pagetitle'],
+                    'lang' => $lang,
+                    'id' => $OwnerID,
+                    'subject' => (!isset($subject)) ? $lang['mess_no_subject'] : $subject,
+                    'to' => $OwnerRecord['username'] . " [" . $OwnerHome['galaxy'] . ":" . $OwnerHome['system'] . ":" . $OwnerHome['planet'] . "]",
+                    'text' => @$text,
+                ));
+
                 if ($game_config['enable_bbcode'] == 1) {
-                    $page .= parsetemplate(gettemplate('messages_pm_form_bb'), $parse);
+
+                    $this->render('messages_pm_form_bb.tpl');
                 } else {
-                    $page .= parsetemplate(gettemplate('messages_pm_form'), $parse);
+
+                    $this->render('messages_pm_form.tpl');
                 }
                 break;
 
@@ -176,171 +171,51 @@ class ShowMessagesPage extends AbstractGamePage {
             case 'show':
                 // -------------------------------------------------------------------------------------------------------
                 // Affichage de la page des messages
-                $page = "<script language=\"JavaScript\">\n";
-                $page .= "function f(target_url, win_name) {\n";
-                $page .= "var new_win = window.open(target_url,win_name,'resizable=yes,scrollbars=yes,menubar=no,toolbar=no,width=550,height=280,top=0,left=0');\n";
-                $page .= "new_win.focus();\n";
-                $page .= "}\n";
-                $page .= "</script>\n";
-                $page .= "<center>";
-                $page .= "<table>";
-                $page .= "<tr>";
-                $page .= "<td></td>";
-                $page .= "<td>\n";
-                $page .= "<table width=\"519\">";
-                $page .= "<form action=\"game.php?page=messages\" method=\"post\"><table>";
-                $page .= "<tr>";
-                $page .= "<td></td>";
-                $page .= "<td>\n<input name=\"messages\" value=\"1\" type=\"hidden\">";
-                $page .= "<table width=\"519\">";
-                $page .= "<tr>";
-                $page .= "<th colspan=\"4\">";
-                $page .= "<select onchange=\"document.getElementById('deletemessages').options[this.selectedIndex].selected='true'\" id=\"deletemessages2\" name=\"deletemessages2\">";
-                $page .= "<option value=\"deletemarked\">" . $lang['mess_deletemarked'] . "</option>";
-                $page .= "<option value=\"deleteunmarked\">" . $lang['mess_deleteunmarked'] . "</option>";
-                $page .= "<option value=\"deleteall\">" . $lang['mess_deleteall'] . "</option>";
-                $page .= "</select>";
-                $page .= "<input value=\"" . $lang['mess_its_ok'] . "\" type=\"submit\">";
-                $page .= "</th>";
-                $page .= "</tr><tr>";
-                $page .= "<th style=\"color: rgb(242, 204, 74);\" colspan=\"4\">";
-                $page .= "<input name=\"category\" value=\"" . $MessCategory . "\" type=\"hidden\">";
-                $page .= "<input onchange=\"document.getElementById('fullreports').checked=this.checked\" id=\"fullreports2\" name=\"fullreports2\" type=\"checkbox\">" . $lang['mess_partialreport'] . "</th>";
-                $page .= "</tr><tr>";
-                $page .= "<th>" . $lang['mess_action'] . "</th>";
-                $page .= "<th>" . $lang['mess_date'] . "</th>";
-                $page .= "<th>" . $lang['mess_from'] . "</th>";
-                $page .= "<th>" . $lang['mess_subject'] . "</th>";
-                $page .= "</tr>";
-
                 if ($MessCategory == 100) {
-                    $UsrMess = doquery("SELECT * FROM {{table}} WHERE `message_owner` = '" . $user['id'] . "' ORDER BY `message_time` DESC;", 'messages');
                     $SubUpdateQry = "";
                     for ($MessType = 0; $MessType < 101; $MessType++) {
                         if (in_array($MessType, $MessageType)) {
                             $SubUpdateQry .= "`" . $messfields[$MessType] . "` = '0', ";
                         }
                     }
-                    $QryUpdateUser = "UPDATE {{table}} SET ";
-                    $QryUpdateUser .= $SubUpdateQry;
-                    $QryUpdateUser .= "`id` = '" . $user['id'] . "' ";
-                    $QryUpdateUser .= "WHERE ";
-                    $QryUpdateUser .= "`id` = '" . $user['id'] . "';";
+                    $QryUpdateUser = "UPDATE {{table}} SET $SubUpdateQry`id` = '" . $user['id'] . "' WHERE `id` = '" . $user['id'] . "';";
                     doquery($QryUpdateUser, 'users');
-
-                    while ($CurMess = mysqli_fetch_array($UsrMess)) {
-                        $page .= "\n<tr>";
-                        $page .= "<input name=\"showmes" . $CurMess['message_id'] . "\" type=\"hidden\" value=\"1\">";
-                        $page .= "<th><input name=\"delmes" . $CurMess['message_id'] . "\" type=\"checkbox\"></th>";
-                        $page .= "<th>" . date("m-d H:i:s O", $CurMess['message_time']) . "</th>";
-                        $page .= "<th>" . stripslashes($CurMess['message_from']) . "</th>";
-                        $page .= "<th>" . stripslashes($CurMess['message_subject']) . " ";
-                        if ($CurMess['message_type'] == 1) {
-                            $page .= "<a href=\"game.php?page=messages&type=write&amp;id=" . $CurMess['message_sender'] . "&amp;subject=" . $lang['mess_answer_prefix'] . htmlspecialchars($CurMess['message_subject']) . "\">";
-                            $page .= "<img src=\"" . $dpath . "img/m.gif\" alt=\"" . $lang['mess_answer'] . "\" border=\"0\"></a></th>";
-                        } else {
-                            $page .= "</th>";
-                        }
-                        $page .= "</tr><tr>";
-                        $page .= "<td style=\"background-color: " . $BackGndColor[$CurMess['message_type']] . "; background-image: none;\"; class=\"b\"> </td>";
-                        $page .= "<td style=\"background-color: " . $BackGndColor[$CurMess['message_type']] . "; background-image: none;\"; colspan=\"3\" class=\"b\">" . stripslashes(nl2br($CurMess['message_text'])) . "</td>";
-                        $page .= "</tr>";
-                    }
                 } else {
-                    $UsrMess = doquery("SELECT * FROM {{table}} WHERE `message_owner` = '" . $user['id'] . "' AND `message_type` = '" . $MessCategory . "' ORDER BY `message_time` DESC;", 'messages');
                     if ($WaitingMess[$MessCategory] <> '') {
-                        $QryUpdateUser = "UPDATE {{table}} SET ";
-                        $QryUpdateUser .= "`" . $messfields[$MessCategory] . "` = '0', ";
-                        $QryUpdateUser .= "`" . $messfields[100] . "` = `" . $messfields[100] . "` - '" . $WaitingMess[$MessCategory] . "' ";
-                        $QryUpdateUser .= "WHERE ";
-                        $QryUpdateUser .= "`id` = '" . $user['id'] . "';";
+                        $QryUpdateUser = "UPDATE {{table}} SET `" . $messfields[$MessCategory] . "` = '0', `" . $messfields[100] . "` = `" . $messfields[100] . "` - '" . $WaitingMess[$MessCategory] . "' WHERE `id` = '" . $user['id'] . "';";
                         doquery($QryUpdateUser, 'users');
-                    }
-                    while ($CurMess = mysqli_fetch_array($UsrMess)) {
-                        if ($CurMess['message_type'] == $MessCategory) {
-                            $page .= "\n<tr>";
-                            $page .= "<input name=\"showmes" . $CurMess['message_id'] . "\" type=\"hidden\" value=\"1\">";
-                            $page .= "<th><input name=\"delmes" . $CurMess['message_id'] . "\" type=\"checkbox\"></th>";
-                            $page .= "<th>" . date("m-d H:i:s O", $CurMess['message_time']) . "</th>";
-                            $page .= "<th>" . stripslashes($CurMess['message_from']) . "</th>";
-                            $page .= "<th>" . stripslashes($CurMess['message_subject']) . " ";
-                            if ($CurMess['message_type'] == 1) {
-                                $page .= "<a href=\"game.php?page=messages&type=write&amp;id=" . $CurMess['message_sender'] . "&amp;subject=" . $lang['mess_answer_prefix'] . htmlspecialchars($CurMess['message_subject']) . "\">";
-                                $page .= "<img src=\"" . $dpath . "img/m.gif\" alt=\"" . $lang['mess_answer'] . "\" border=\"0\"></a></th>";
-                            } else {
-                                $page .= "</th>";
-                            }
-                            $page .= "</tr><tr>";
-                            $page .= "<td class=\"b\"> </td>";
-                            $page .= "<td colspan=\"3\" class=\"b\">" . nl2br(stripslashes($CurMess['message_text'])) . "</td>";
-                            $page .= "</tr>";
-                        }
                     }
                 }
 
+                $this->tplObj->assign(array(
+                    'title' => $lang['mess_pagetitle'],
+                    'lang' => $lang,
+                    'UsrMess' => doquery("SELECT * FROM {{table}} WHERE `message_owner` = '" . $user['id'] . "' ORDER BY `message_time` DESC;", 'messages'),
+                    'UsrMess1' => doquery("SELECT * FROM {{table}} WHERE `message_owner` = '" . $user['id'] . "' AND `message_type` = '" . $MessCategory . "' ORDER BY `message_time` DESC;", 'messages'),
+                    'MessCategory' => $MessCategory,
+                    'MessType' => $MessType,
+                    'MessageType' => $MessageType,
+                    'BackGndColor' => array(0 => '#663366', 1 => '#336666', 2 => '#000099', 3 => '#666666', 4 => '#999999', 5 => '#999999', 15 => '#999999', 99 => '#999999', 100 => '#999999'),
+                ));
 
-                $page .= "<tr>";
-                $page .= "<th style=\"color: rgb(242, 204, 74);\" colspan=\"4\">";
-                $page .= "<input onchange=\"document.getElementById('fullreports2').checked=this.checked\" id=\"fullreports\" name=\"fullreports\" type=\"checkbox\">" . $lang['mess_partialreport'] . "</th>";
-                $page .= "</tr><tr>";
-                $page .= "<th colspan=\"4\">";
-                $page .= "<select onchange=\"document.getElementById('deletemessages2').options[this.selectedIndex].selected='true'\" id=\"deletemessages\" name=\"deletemessages\">";
-                $page .= "<option value=\"deletemarked\">" . $lang['mess_deletemarked'] . "</option>";
-                $page .= "<option value=\"deleteunmarked\">" . $lang['mess_deleteunmarked'] . "</option>";
-                $page .= "<option value=\"deleteall\">" . $lang['mess_deleteall'] . "</option>";
-                $page .= "</select>";
-                $page .= "<input value=\"" . $lang['mess_its_ok'] . "\" type=\"submit\">";
-                $page .= "</th>";
-                $page .= "</tr><tr>";
-                $page .= "<td colspan=\"4\"></td>";
-                $page .= "</tr>";
-                $page .= "</table>\n";
-                $page .= "</td>";
-                $page .= "</tr>";
-                $page .= "</table>\n";
-                $page .= "</form>";
-                $page .= "</td>";
-                $page .= "</table>\n";
-                $page .= "</center>";
+                $this->render('messages_show.tpl');
                 break;
 
             default:
-                $page = "<script language=\"JavaScript\">\n";
-                $page .= "function f(target_url, win_name) {\n";
-                $page .= "var new_win = window.open(target_url, win_name, 'resizable=yes, scrollbars=yes, menubar=no, toolbar=no, width=550, height=280, top=0, left=0');\n";
-                $page .= "new_win.focus();\n";
-                $page .= "}\n";
-                $page .= "</script>\n";
-                $page .= "<center>";
-                $page .= "<br>";
-                $page .= "<table width=\"569\">";
-                $page .= "<tr>";
-                $page .= "	<td class=\"c\" colspan=\"5\">" . $lang['title'] . "</td>";
-                $page .= "</tr><tr>";
-                $page .= "	<th colspan=\"3\">" . $lang['head_type'] . "</th>";
-                $page .= "	<th>" . $lang['head_count'] . "</th>";
-                $page .= "	<th>" . $lang['head_total'] . "</th>";
-                $page .= "</tr>";
-                $page .= "<tr>";
-                $page .= "	<th colspan=\"3\"><a href=\"game.php?page=messages&type=show&amp;messcat=100\"><font color=\"" . $TitleColor[100] . "\">" . $lang['type'][100] . "</a></th>";
-                $page .= "	<th><font color=\"" . $TitleColor[100] . "\">" . $WaitingMess[100] . "</font></th>";
-                $page .= "	<th><font color=\"" . $TitleColor[100] . "\">" . $TotalMess[100] . "</font></th>";
-                $page .= "</tr>";
-                for ($MessType = 0; $MessType < 100; $MessType++) {
-                    if (in_array($MessType, $MessageType)) {
-                        $page .= "<tr>";
-                        $page .= "	<th colspan=\"3\"><a href=\"game.php?page=messages&type=show&amp;messcat=" . $MessType . " \"><font color=\"" . $TitleColor[$MessType] . "\">" . $lang['type'][$MessType] . "</a></th>";
-                        $page .= "	<th><font color=\"" . $TitleColor[$MessType] . "\">" . $WaitingMess[$MessType] . "</font></th>";
-                        $page .= "	<th><font color=\"" . $TitleColor[$MessType] . "\">" . $TotalMess[$MessType] . "</font></th>";
-                        $page .= "</tr>";
-                    }
-                }
-                $page .= "</table>";
-                $page .= "</center>";
+
+                $this->tplObj->assign(array(
+                    'title' => $lang['mess_pagetitle'],
+                    'lang' => $lang,
+                    'TitleColor' => $TitleColor,
+                    'WaitingMess' => $WaitingMess,
+                    'TotalMess' => $TotalMess,
+                    'MessType' => $MessType,
+                    'MessageType' => $MessageType,
+                ));
+
+                $this->render('messages_default.tpl');
                 break;
         }
-
-        display($page, $lang['mess_pagetitle']);
     }
 
 }
