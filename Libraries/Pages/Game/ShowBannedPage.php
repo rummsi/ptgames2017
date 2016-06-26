cc4c1bf3f1dc5c96f2707068c8b4a3718f2441f6
rummsi <rummsi@gmail.com>
2016-06-26 14:46:00 +0000

    SMARTY: ShowBannedPage.

--- Libraries/Pages/Game/ShowBannedPage.php
+++ Libraries/Pages/Game/ShowBannedPage.php
@@ -40,29 +40,13 @@ class ShowBannedPage extends AbstractGamePage {
 
         includeLang('banned');
 
-        $parse = $lang;
-        $parse['dpath'] = $dpath;
-        $parse['mf'] = $mf;
-
-
-        $query = doquery("SELECT * FROM {{table}} ORDER BY `id`;", 'banned');
-        $i = 0;
-        while ($u = mysqli_fetch_array($query)) {
-            $parse['banned'] .=
-                    "<tr><td class=b><center><b>" . $u[1] . "</center></td></b>" .
-                    "<td class=b><center><b>" . $u[2] . "</center></b></td>" .
-                    "<td class=b><center><b>" . date("d/m/Y G:i:s", $u[4]) . "</center></b></td>" .
-                    "<td class=b><center><b>" . date("d/m/Y G:i:s", $u[5]) . "</center></b></td>" .
-                    "<td class=b><center><b>" . $u[6] . "</center></b></td></tr>";
-            $i++;
-        }
-
-        if ($i == "0")
-            $parse['banned'] .= "<tr><th class=b colspan=6>Il n'y a pas de joueurs bannis</th></tr>";
-        else
-            $parse['banned'] .= "<tr><th class=b colspan=6>Il y a {$i} joueurs bannis</th></tr>";
-
-        display(parsetemplate(gettemplate('banned_body'), $parse), 'Banned', true);
+        $this->tplObj->assign(array(
+            'title' => "Banned",
+            'lang' => $lang,
+            'query' => doquery("SELECT * FROM {{table}} ORDER BY `id`;", 'banned'),
+            'i' => 0,
+        ));
+        $this->render('banned_body.tpl');
     }
 
 }
